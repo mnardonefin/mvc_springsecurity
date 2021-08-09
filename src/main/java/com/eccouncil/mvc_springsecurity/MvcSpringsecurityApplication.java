c096a0afcdefbaf695f26e1e6d63f3872a23e51a
mnardonefin <massimonardonedevchannel@gmail.com>
2021-08-09 16:07:37 +0300

    Adding Practical Spring security Section 2 project

---
+++ src/main/java/com/eccouncil/mvc_springsecurity/MvcSpringsecurityApplication.java
@@ -0,0 +1,13 @@
+package com.eccouncil.mvc_springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcSpringsecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcSpringsecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eccouncil/mvc_springsecurity/MvcSpringsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eccouncil.mvc_springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcSpringsecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
